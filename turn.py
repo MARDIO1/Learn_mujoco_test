cee47492f5fdf2a141f04c092e484824b1295511
mardio <mardio@noreply.gitcode.com>
2025-09-19 21:48:19 +0800

    转化urdf出现一些问题；mujoco有什么好处？空气动力学又不是自带的

---
+++ turn.py
@@ -0,0 +1,7 @@
+from urdf2mjcf import run
+
+run(
+    urdf_path=r"T:\Learn_More\learn from project\mujocu\try.urdf",
+    mjcf_path=r"T:\Learn_More\learn from project\mujocu\try.mjcf",
+    copy_meshes=True,
+)
